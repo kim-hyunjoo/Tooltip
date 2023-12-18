05e57d61a7c18a3a16833c36f026a8451034727a
kim-hyunjoo <khj9x.x@gmail.com>
2023-12-18 18:22:48 +0900

    feat: 프로젝트 기본 패키지 설치

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  background-color: red;
+`
+
+function App() {
+  return (
+    <div className="App">
+      <Container>Hello</Container>
+    </div>
+  );
+}
+
+export default App;
